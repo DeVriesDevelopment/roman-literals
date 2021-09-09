69ba9c9cb2f6d4163ec6b15cf3da22487fa99a84
Bart de Vries <bart.devries@triodos.com>
2021-09-09 19:52:57 +0200

    -

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,27 +1,48 @@
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class Converter {
+class Converter {
 
-  static final Map<Integer, String> romanNumerals = new TreeMap<>(());
+  private static final Map<Integer, String> ROMAN_NUMERALS = new TreeMap<>(Comparator.reverseOrder());
+  private static final Map<String, String> ROMAN_NUMERALS_REPLACEMENT_MAPPER = new LinkedHashMap<>();
 
   static {
-    romanNumerals.put(1, "I");
-    romanNumerals.put(5, "V");
-    romanNumerals.put(10, "X");
-    romanNumerals.put(50, "L");
+    ROMAN_NUMERALS.put(1, "I");
+    ROMAN_NUMERALS.put(5, "V");
+    ROMAN_NUMERALS.put(10, "X");
+    ROMAN_NUMERALS.put(50, "L");
+    ROMAN_NUMERALS.put(100, "C");
+    ROMAN_NUMERALS.put(500, "D");
+    ROMAN_NUMERALS.put(1000, "M");
+
+    ROMAN_NUMERALS_REPLACEMENT_MAPPER.put("DCCCC", "CM");
+    ROMAN_NUMERALS_REPLACEMENT_MAPPER.put("CCCC", "CD");
+    ROMAN_NUMERALS_REPLACEMENT_MAPPER.put("LXXXX", "XC");
+    ROMAN_NUMERALS_REPLACEMENT_MAPPER.put("XXXX", "XL");
+    ROMAN_NUMERALS_REPLACEMENT_MAPPER.put("VIIII", "IX");
+    ROMAN_NUMERALS_REPLACEMENT_MAPPER.put("IIII", "IV");
   }
 
-  public static String convert(int i) {
-    if (i == 5) {
-      return "V";
-    } if (i == 10) {
-      return "X";
-    } if (i == 50) {
-      return "L";
-    } else {
-      return "I".repeat(i);
+  static String convert(int i) {
+    if(i >= 4000 || i < 1){
+      throw new IllegalArgumentException();
     }
+
+    int remaining = i;
+    String romanNumeral = "";
+    while(remaining > 0) {
+      int finalRemaining = remaining;
+      int biggestNumeral = ROMAN_NUMERALS.keySet().stream().filter(k -> finalRemaining >= k).findFirst().orElse(1);
+      romanNumeral += (ROMAN_NUMERALS.get(biggestNumeral));
+      remaining -= biggestNumeral;
+    }
+    System.out.println(romanNumeral);
+    for (Map.Entry<String, String> replacement: ROMAN_NUMERALS_REPLACEMENT_MAPPER.entrySet()) {
+      romanNumeral = romanNumeral.replace(replacement.getKey(), replacement.getValue());
+    };
+    return romanNumeral;
   }
 }
--- src/test/java/RomanLiteralsTest.java
+++ src/test/java/RomanLiteralsTest.java
@@ -1,6 +1,10 @@
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class RomanLiteralsTest {
 
@@ -8,9 +12,15 @@ public class RomanLiteralsTest {
     return Converter.convert(i);
   }
 
+  static Map<String, Integer> parameters = new HashMap<>();
+
+  static {
+    parameters.put("I", 1);
+  }
+
   @Test
-  public void TestConvertOneToRomanLiterals() {
-    assertEquals("I", convert(1));
+  public void parameterizedRomanNumeralTest(){
+    parameters.forEach((String key, Integer value) -> assertEquals(key, convert(1)));
   }
 
   @Test
@@ -36,5 +46,75 @@ public class RomanLiteralsTest {
   @Test
   public void TestConvertFiftyToRomanLiterals() {
     assertEquals("L", convert(50));
+  }
+
+  @Test
+  public void TestConvertHundredToRomanLiterals() {
+    assertEquals("C", convert(100));
+  }
+
+  @Test
+  public void TestConverterFiveHundredToRomanLiterals() {
+    assertEquals("D", convert(500));
+  }
+
+  @Test
+  public void TestConverterThousandToRomanLiterals() {
+    assertEquals("M", convert(1000));
+  }
+
+  @Test
+  public void TestConverterFourToRomanLiterals() {
+    assertEquals("IV", convert(4));
+  }
+
+  @Test
+  public void TestConverterSixToRomanLiterals() {
+    assertEquals("VI", convert(6));
+  }
+
+  @Test
+  public void TestConverterNineToRomanLiterals() {
+    assertEquals("IX", convert(9));
+  }
+
+  @Test
+  public void TestConverterFourteenToRomanLiterals() {
+    assertEquals("XIV", convert(14));
+  }
+
+  @Test
+  public void TestConverterFourtynineToRomanLiterals() {
+    assertEquals("XLIX", convert(49));
+  }
+
+  @Test
+  public void TestConverterNinetynineToRomanLiterals() {
+    assertEquals("XCIX", convert(99));
+  }
+
+  @Test
+  public void TestConverterFourNinetyNineToRomanLiterals() {
+    assertEquals("CDXCIX", convert(499));
+  }
+
+  @Test
+  public void TestConverterNineHunderdNinetyNineToRomanLiterals() {
+    assertEquals("CMXCIX", convert(999));
+  }
+
+  @Test
+  public void TestConverterThreeThousandNineHundredNinetyNineToRomanLiterals() {
+    assertEquals("MMMCMXCIX", convert(3999));
+  }
+
+  @Test
+  public void Test(){
+    assertEquals("MDCLXVII", convert(1667));
+  }
+
+  @Test
+  public void TestConverterFourThousand(){
+    assertThrows(IllegalArgumentException.class, () -> convert(4000));
   }
 }
